6f7a69cf18a61b7cedd3d404c7788ac28f410682
Ryan Skinner <Ryan Skinner@10.0.0.148>
2015-10-20 01:02:44 -0600

    Hey guys,
    This is Tanner and My Code. I think I got all the important things done.
    Tanner is working on getting his Junit testcases set up.

---
+++ PizzaApp/src/JunitTests/PersonTest.java
@@ -0,0 +1,45 @@
+package JunitTests;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Array;
+
+import org.junit.Test;
+
+import Main.Address;
+import Main.Person;
+
+public class PersonTest {
+
+	@Test
+	public void testEquals() {
+		Person[] people = generatePeople();
+		
+		assertTrue(people[0].equals(people[2]));
+		assertFalse(people[0].equals(people[1]));
+		
+		System.out.println(people[0].toString());
+	}
+	
+	@Test
+	public void testPlaceOrder(){
+		
+		Person jon = new Person("jon nobody", "jonisAnobody@gmail.com", "3033331517");
+		assertTrue(jon.placeOrder() >= 0);
+	}
+	
+	//generates an array of people to be used in a test class
+	public static Person[] generatePeople(){
+				
+		Address sameAsSteve = new Address("2584 stevie dr", null, "Steville", "CO", "808080", "USA");
+		
+		Person jon = new Person("jon nobody", "jonisAnobody@gmail.com", "3033331517");
+		Person steve = new Person("steve smeve", "stevethinksHesgreat@ymail.com", "1112223334", "2584 stevie dr", null, "Steville", "CO", "808080", "USA");
+		Person jonAgain = new Person("jon nobody", "jonisAnobody@gmail.com", "3033331517");
+		Person connie = new Person("connie smeve", "connieIsSmevesWife@match.com", "1112223334", sameAsSteve);
+		
+		Person[] people = {jon, steve, jonAgain, connie};
+		return people;
+	}
+
+}
