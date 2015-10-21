ec79a02ba94721e13ef1041ca4cda7dd310022ab
Tanner <Tanner@Mini-Trogdor>
2015-10-20 18:13:07 -0600

    Junit Tests for Menu

---
+++ PizzaApp/src/JunitTests/MenuTest.java
@@ -0,0 +1,130 @@
+package JunitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Main.Menu;
+import Main.MenuItem;
+
+
+public class MenuTest {
+
+	@Test
+	public void testAddMenuItem() {
+
+		Menu menu = new Menu();
+
+		MenuItem cheese = new MenuItem("cheese Pizza", 9.99);
+		MenuItem pepperoni = new MenuItem("Pepperoni Pizza", 10.99);
+		MenuItem MeatLovers = new MenuItem("Meat Lovers Pizza", 12.99);
+		MenuItem veggie = new MenuItem("veggie Pizza", 10.99);
+		MenuItem sausage = new MenuItem("Sausage Pizza", 11.99);
+
+
+		menu.addMenuItem(cheese);
+		menu.addMenuItem(pepperoni);
+		menu.addMenuItem(MeatLovers);
+		menu.addMenuItem(veggie);
+		menu.addMenuItem(sausage);
+
+		assertEquals(5,menu.getItems().size());
+
+	}
+	@Test
+	public void testAddMenuItem1() {
+
+		Menu menu = new Menu();
+
+		MenuItem cheese = new MenuItem("cheese Pizza", 9.99);
+
+		menu.addMenuItem(cheese);
+
+		assertEquals(1,menu.getItems().size());
+
+	}
+	@Test
+	public void testAddMenuItem2() {
+
+		Menu menu = new Menu();
+
+		MenuItem cheese = new MenuItem("cheese Pizza", 9.99);
+		MenuItem pepperoni = new MenuItem("Pepperoni Pizza", 10.99);
+		MenuItem MeatLovers = new MenuItem("Meat Lovers Pizza", 12.99);
+		MenuItem veggie = new MenuItem("veggie Pizza", 10.99);
+		MenuItem sausage = new MenuItem("Sausage Pizza", 11.99);
+
+
+		menu.addMenuItem(cheese);
+		menu.addMenuItem(pepperoni);
+		menu.addMenuItem(MeatLovers);
+		menu.addMenuItem(veggie);
+		menu.addMenuItem(sausage);
+		menu.addMenuItem(sausage);
+		
+
+		assertEquals(5,menu.getItems().size());
+
+	}
+	@Test
+	public void testRemoveMenuItem() {
+
+		Menu menu = new Menu();
+
+		MenuItem cheese = new MenuItem("cheese Pizza", 9.99);
+		menu.addMenuItem(cheese);
+		assertEquals(1,menu.getItems().size());
+		menu.removeMenuItem(cheese);
+		assertEquals(0, menu.getItems().size());
+
+	}
+	@Test
+	public void testRemoveMenuItem1() {
+
+		Menu menu = new Menu();
+
+		MenuItem cheese = new MenuItem("cheese Pizza", 9.99);
+		MenuItem pepperoni = new MenuItem("Pepperoni Pizza", 10.99);
+		MenuItem MeatLovers = new MenuItem("Meat Lovers Pizza", 12.99);
+		MenuItem veggie = new MenuItem("veggie Pizza", 10.99);
+		MenuItem sausage = new MenuItem("Sausage Pizza", 11.99);
+
+
+		menu.addMenuItem(cheese);
+		menu.addMenuItem(pepperoni);
+		menu.addMenuItem(MeatLovers);
+		menu.addMenuItem(veggie);
+		menu.addMenuItem(sausage);
+		menu.removeMenuItem(pepperoni);
+		
+
+		assertEquals(4,menu.getItems().size());
+
+	}
+	public void testRemoveMenuItem2() {
+
+		Menu menu = new Menu();
+
+		MenuItem cheese = new MenuItem("cheese Pizza", 9.99);
+		MenuItem pepperoni = new MenuItem("Pepperoni Pizza", 10.99);
+		MenuItem MeatLovers = new MenuItem("Meat Lovers Pizza", 12.99);
+		MenuItem veggie = new MenuItem("veggie Pizza", 10.99);
+		MenuItem sausage = new MenuItem("Sausage Pizza", 11.99);
+
+
+		menu.addMenuItem(cheese);
+		menu.addMenuItem(pepperoni);
+		menu.addMenuItem(MeatLovers);
+		menu.addMenuItem(veggie);
+		menu.addMenuItem(sausage);
+		menu.removeMenuItem(pepperoni);
+		menu.removeMenuItem(pepperoni);
+		
+
+		assertEquals(4,menu.getItems().size());
+
+	}
+	
+	
+
+}
--- PizzaApp/src/Main/Menu.java
+++ PizzaApp/src/Main/Menu.java
@@ -5,9 +5,18 @@ import java.util.Set;
 
 public class Menu {
 
-	static Set<MenuItem> items = new HashSet<MenuItem>();
+	Set<MenuItem> items = new HashSet<MenuItem>();
 	
-	public static void addMenuItem(MenuItem item){
+	public Menu(){
+		
+	}
+	public Menu(Set<MenuItem> i){
+		
+		items=i;
+		
+	}
+	
+	public void addMenuItem(MenuItem item){
 		items.add(item);		
 	}
 	
@@ -16,27 +25,6 @@ public class Menu {
 	}
 	public Set<MenuItem> getItems(){
 		return items;
-	}
-	
-	public static void main(String[] args){
-				
-		MenuItem cheese = new MenuItem("cheese Pizza", 9.99);
-		MenuItem pepperoni = new MenuItem("Pepperoni Pizza", 10.99);
-		MenuItem MeatLovers = new MenuItem("Meat Lovers Pizza", 12.99);
-		MenuItem veggie = new MenuItem("veggie Pizza", 10.99);
-		MenuItem sausage = new MenuItem("Sausage Pizza", 11.99);
-		
-		addMenuItem(cheese);
-		addMenuItem(pepperoni);
-		addMenuItem(MeatLovers);
-		addMenuItem(veggie);
-		addMenuItem(sausage);
-		
-		
-		
-		System.out.println(items.size());
-		
-		
 	}
 	
 }
