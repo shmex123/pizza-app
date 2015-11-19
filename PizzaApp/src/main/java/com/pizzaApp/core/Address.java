4fd77b821f17fdcbc3d477625e62f74d972aef48
rskinner956 <rskinner956@users.noreply.github.com>
2015-11-18 17:12:28 -0700

    Update Address.java
    Address is now a bean

--- PizzaApp/src/main/java/com/pizzaApp/core/Address.java
+++ PizzaApp/src/main/java/com/pizzaApp/core/Address.java
@@ -3,12 +3,12 @@ package com.pizzaApp.core;
 
 public class Address {
 	
-	String address1;
-	String address2;
-	String city;
-	String locality;
-	String postalCode;
-	String country;
+	private String address1;
+	private String address2;
+	private String city;
+	private String locality;
+	private String postalCode;
+	private String country;
 	
 	
 	public Address (String address1, String address2, String city, String locality, String postalCode, String country){
@@ -37,7 +37,54 @@ public class Address {
 		}
 		str += city + ", " + locality + ", " + postalCode + "\n";
 		return str;
-		
+	}
+	
+	public String getAddress1() {
+		return address1;
+	}
+	
+	public void setAddress1(String address1){
+		this.address1 = address1;
+	}
+	
+	public String getAddress2() {
+		return address2;
+	}
+	
+	public void setAddress2(String address2){
+		this.address2 = address2;
+	}
+	
+	public String getCity(){
+		return city;
+	}
+	
+	public void setCity(String city){
+		this.city = city;
+	}
+	
+	public String getLocality(){
+		return locality;
+	}
+	
+	public void setLocality(String locality){
+		this.locality = locality;
+	}
+	
+	public String getPostalCode(){
+		return postalCode;
+	}
+	
+	public void setPostalCode(String postalCode){
+		this.postalCode = postalCode;
+	}
+	
+	public String getCountry(){
+		return country;
+	}
+	
+	public void setCountry(String country){
+		this.country = country;
 	}
 
 }
